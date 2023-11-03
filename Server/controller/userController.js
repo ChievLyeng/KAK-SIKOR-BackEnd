fed9d9e215c012196cafb0d149b304d6a3a02399
027 <anbschool0027@027s-Mac-mini.local>
2023-11-03 16:40:16 +0700

    create user model

---
+++ Server/controller/userController.js
@@ -0,0 +1,33 @@
+import User from "./../models/userModel.js";
+
+//  user registration
+export const registerUser = async (req, res) => {
+  try {
+    const newUser = new User(req.body);
+    const savedUser = await newUser.save();
+    res.status(201).json(savedUser);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+//  user login
+export const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // Find the user by email
+    const user = await User.findOne({ email });
+
+    // Check if the user exists and if the password is correct
+    if (user && (await user.matchPassword(password))) {
+      res.status(200).json({ message: "Login successful", user });
+    } else {
+      res.status(401).json({ error: "Invalid email or password" });
+    }
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export default { registerUser, loginUser };
