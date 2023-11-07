7a1ce0bdf5aea0efa8be2c4e68485abaf260732d
027 <anbschool0027@027s-Mac-mini.local>
2023-11-07 17:05:56 +0700

    update user schema

--- Server/models/userModel.js
+++ Server/models/userModel.js
@@ -1,58 +1,119 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
+const bcrypt = require("bcryptjs");
 
-const userSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: [true, "Username is required."],
-    unique: true,
-    trim: true,
-    minlength: [3, "Username must be at least 3 characters long."],
-    maxlength: [30, "Username cannot exceed 30 characters."],
-  },
-  email: {
-    type: String,
-    required: [true, "Email is required."],
-    unique: true,
-    trim: true,
-    maxlength: [255, "Email cannot exceed 255 characters."],
-    validate: {
-      validator: (value) => /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value),
-      message: "Invalid email format.",
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: [true, "First name is required."],
+      trim: true,
+      maxlength: [30, "First name cannot exceed 30 characters."],
     },
-  },
-  phoneNumber: {
-    type: Number,
-    required: [true, "Phone number is required."],
-    unique: true,
-    validate: {
-      validator: function (value) {
-        return /^\d+$/.test(value);
+    lastName: {
+      type: String,
+      required: [true, "Last name is required."],
+      trim: true,
+      maxlength: [30, "Last name cannot exceed 30 characters."],
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required."],
+      unique: true,
+      trim: true,
+      maxlength: [255, "Email cannot exceed 255 characters."],
+      validate: {
+        validator: (value) => /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value),
+        message: "Invalid email format.",
       },
-      message: "Phone number can only contain numeric characters.",
     },
-  },
-  birthdate: {
-    type: Date,
-    required: [true, "Birthdate is required."],
-  },
-  gender: {
-    type: String,
-    enum: ["Male", "Female", "Other"],
-    required: [true, "Gender is required."],
-  },
-  password: {
-    type: String,
-    required: [true, "Password is required."],
-  },
-  confirmPassword: {
-    type: String,
-    validate: {
-      validator: function (value) {
-        return value === this.password;
+    phoneNumber: {
+      type: Number,
+      required: [true, "Phone number is required."],
+      unique: true,
+      validate: {
+        validator: function (value) {
+          return /^\d+$/.test(value);
+        },
+        message: "Phone number can only contain numeric characters.",
       },
-      message: "Passwords do not match.",
+    },
+    birthDate: {
+      type: Date,
+      required: [true, "Birthdate is required."],
+    },
+    gender: {
+      type: String,
+      enum: ["Male", "Female", "Other"],
+      required: [true, "Gender is required."],
+    },
+    address: {
+      city: {
+        type: String,
+        trim: true,
+      },
+      commune: {
+        type: String,
+        trim: true,
+      },
+      district: {
+        type: String,
+        trim: true,
+      },
+      village: {
+        type: String,
+        trim: true,
+      },
+      homeNumber: {
+        type: Number,
+        trim: true,
+      },
+      street: {
+        type: String,
+        trim: true,
+      },
+    },
+    profilePicture: String,
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      validate: {
+        validator: (value) => {
+          return validator.isStrongPassword(value, {
+            minLength: 8,
+            minLowercase: 1,
+            minUppercase: 1,
+            minNumbers: 1,
+            minSymbols: 1,
+          });
+        },
+        message: "Password must be strong and secure",
+      },
+    },
+    confirmPassword: {
+      type: String,
+      validate: {
+        validator: function (value) {
+          return value === this.password;
+        },
+        message: "Passwords do not match.",
+      },
     },
   },
+  {
+    timestamps: true,
+  }
+);
+
+userSchema.pre("save", async function (next) {
+  // Only run this function if password was actually modified
+  if (!this.isModified("password")) return next();
+
+  this.password = await bcrypt.hash(this.password, 12);
+
+  // Delete passwordConfirm field
+  this.confirmPassword = undefined;
+  next();
 });
 
 const User = mongoose.model("User", userSchema);
