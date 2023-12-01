ed57e21f997963fe7ccdaf229089c50c60becd80
Lyeng <lyeng2210@gmail.com>
2023-12-01 20:13:51 +0700

    create global error handling

--- Server/server.js
+++ Server/server.js
@@ -7,6 +7,8 @@ const userRoute = require("./routes/userRoute");
 const orderRoute = require("./routes/orderRoute");
 const cors = require("cors");
 const morgan = require("morgan");
+const AppError = require('./utils/appError')
+const globalErrorhandler = require('./middlewares/globalErrorhandler')
 require("dotenv").config();
 
 const ReviewRoute = require("./routes/reviewRoute");
@@ -22,6 +24,10 @@ app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.all("*", (req, res, next) => {
+  next(new AppError(`Cant't find ${req.originalUrl} on this server!`, 404));
+});
+app.use(globalErrorhandler);
 
 // routes
 app.use("/reviews", ReviewRoute);
