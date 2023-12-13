bcda88d9037867b4529955a84a7d2ca865241e29
vannlithi <vannlithi168@gmail.com>
2023-12-13 15:16:34 +0700

    adding restrict to role

--- Server/middlewares/authMiddleware.js
+++ Server/middlewares/authMiddleware.js
@@ -63,4 +63,15 @@ const requireAdmin = asyncHandler(async (req, res, next) => {
   }
 });
 
-module.exports = { requireSignIn, requireAdmin };
+const restrictTo =
+  (...roles) =>
+  (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return next(
+        new AppError("You do not have permission to perform this action", 403)
+      );
+    }
+    next();
+  };
+
+module.exports = { requireSignIn, requireAdmin, restrictTo };
